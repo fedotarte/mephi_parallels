926e1a363f006680ddf8e2dfbde9e8391b0e8a44
fedotarte <artyom.fedotov93@gmail.com>
2021-12-22 02:56:02 +0300

    resolved review comments

--- dijkstra_nodejs/dijkstra.js
+++ dijkstra_nodejs/dijkstra.js
@@ -1,63 +1,94 @@
-const GRAPH_TO_GO = Object.freeze(
-    {
-        start: { A: 5, B: 2 },
-        A: { C: 4, D: 2 },
-        B: { A: 8, D: 7 },
-        C: { D: 6, finish: 3 },
-        D: { finish: 1 },
-        finish: {}
+var VertexNode = /** @class */ (function () {
+    function VertexNode() {
     }
-);
-
-const START_POSITION_NODE = "start";
-const FINISH_POSITION_NODE = "finish";
-
-const lowestCostNode = (costs, processed) => {
-    return Object.keys(costs).reduce((lowest, node) => {
-        if (lowest === null || costs[node] < costs[lowest]) {
-            if (!processed.includes(node)) {
-                lowest = node;
-            }
-        }
-        return lowest;
-    }, null);
-};
-
-const dijkstra = (graph) => {
-    const costs = Object.assign({ finish: Infinity }, graph.start);
-    const parents = { finish: null };
-    for (let child in graph.start) {
-        parents[child] = START_POSITION_NODE;
+    return VertexNode;
+}());
+var Vertex = /** @class */ (function () {
+    function Vertex(theName, theNodes, theWeight) {
+        if (theWeight === void 0) { theWeight = 1; }
+        this.name = theName;
+        this.nodes = theNodes;
+        this.weight = theWeight;
     }
-    const processed = [];
-    let node = lowestCostNode(costs, processed);
-    while (node) {
-        let cost = costs[node];
-        let children = graph[node];
-        Object.keys(children).forEach(n => {
-            const newCost = cost + children[n];
-            if (!costs[n] || costs[n] > newCost) {
-                costs[n] = newCost;
-                parents[n] = node;
-            }
-        })
-        processed.push(node);
-        node = lowestCostNode(costs, processed);
+    return Vertex;
+}());
+var Dijkstra = /** @class */ (function () {
+    function Dijkstra() {
+        this.vertices = {};
     }
-    let optimalPath = [FINISH_POSITION_NODE];
-    let parent = parents.finish;
-    while (parent) {
-        optimalPath.push(parent);
-        parent = parents[parent];
-    }
-    optimalPath.reverse();
-
-    const results = {
-        distance: costs.finish,
-        path: optimalPath
+    Dijkstra.prototype.addVertex = function (vertex) {
+        this.vertices[vertex.name] = vertex;
     };
-
-    return results;
-};
-
-console.log(dijkstra(GRAPH_TO_GO));
+    Dijkstra.prototype.setPathToNodes = function (nodeName, weight) {
+        return { nameOfVertex: nodeName.toUpperCase(), weight: weight };
+    };
+    Dijkstra.prototype.findPointsOfShortestWay = function (start, finish, weight) {
+        var nextVertex = finish;
+        var arrayWithVertex = [];
+        while (nextVertex !== start) {
+            var minWeigth = Number.MAX_VALUE;
+            var minVertex = "";
+            for (var _i = 0, _a = this.vertices[nextVertex].nodes; _i < _a.length; _i++) {
+                var i = _a[_i];
+                if (i.weight + this.vertices[i.nameOfVertex].weight < minWeigth) {
+                    minWeigth = this.vertices[i.nameOfVertex].weight;
+                    minVertex = i.nameOfVertex;
+                }
+            }
+            arrayWithVertex.push(minVertex);
+            nextVertex = minVertex;
+        }
+        return arrayWithVertex;
+    };
+    Dijkstra.prototype.findShortestWay = function (start, finish) {
+        var _this = this;
+        var nodes = {};
+        var visitedVertex = [];
+        for (var i in this.vertices) {
+            if (this.vertices[i].name === start) {
+                this.vertices[i].weight = 0;
+            }
+            else {
+                this.vertices[i].weight = Number.MAX_VALUE;
+            }
+            nodes[this.vertices[i].name] = this.vertices[i].weight;
+        }
+        while (Object.keys(nodes).length !== 0) {
+            var sortedVisitedByWeight = Object.keys(nodes).sort(function (a, b) { return _this.vertices[a].weight - _this.vertices[b].weight; });
+            var currentVertex = this.vertices[sortedVisitedByWeight[0]];
+            for (var _i = 0, _a = currentVertex.nodes; _i < _a.length; _i++) {
+                var j = _a[_i];
+                var calculateWeight = currentVertex.weight + j.weight;
+                if (calculateWeight < this.vertices[j.nameOfVertex].weight) {
+                    this.vertices[j.nameOfVertex].weight = calculateWeight;
+                }
+            }
+            delete nodes[sortedVisitedByWeight[0]];
+        }
+        var finishWeight = this.vertices[finish].weight;
+        var arrayWithVertex = this.findPointsOfShortestWay(start, finish, finishWeight).reverse();
+        arrayWithVertex.push(finish, finishWeight.toString());
+        return arrayWithVertex;
+    };
+    return Dijkstra;
+}());
+var graph = new Dijkstra();
+graph.addVertex(new Vertex("A", [
+    graph.setPathToNodes("B", 3),
+    graph.setPathToNodes("C", 5)
+]));
+graph.addVertex(new Vertex("B", [
+    graph.setPathToNodes("A", 3),
+    graph.setPathToNodes("C", 2),
+    graph.setPathToNodes("D", 6)
+]));
+graph.addVertex(new Vertex("C", [
+    graph.setPathToNodes("B", 2),
+    graph.setPathToNodes("A", 5),
+    graph.setPathToNodes("d", 5)
+]));
+graph.addVertex(new Vertex("D", [
+    graph.setPathToNodes("B", 6),
+    graph.setPathToNodes("C", 5)
+]));
+console.log(graph.findShortestWay("A", "D"));
---
+++ dijkstra_nodejs/dijkstra.ts
@@ -0,0 +1,116 @@
+class VertexNode {
+    nameOfVertex: string;
+    weight: number;
+}
+
+class Vertex {
+    name: string;
+    nodes: VertexNode[];
+    weight: number;
+
+    constructor(theName: string, theNodes: VertexNode[], theWeight: number = 1) {
+        this.name = theName;
+        this.nodes = theNodes;
+        this.weight = theWeight;
+    }
+}
+
+class Dijkstra {
+
+    vertices: any;
+    constructor() {
+        this.vertices = {};
+    }
+
+    addVertex(vertex: Vertex): void {
+        this.vertices[vertex.name] = vertex;
+    }
+
+    setPathToNodes(nodeName: string, weight: number):VertexNode {
+        return {nameOfVertex:nodeName.toUpperCase(), weight}
+    }
+
+    findPointsOfShortestWay(start: string, finish: string, weight: number): string[] {
+
+        let nextVertex: string = finish;
+        let arrayWithVertex: string[] = [];
+        while (nextVertex !== start) {
+
+            let minWeigth: number = Number.MAX_VALUE;
+            let minVertex: string = "";
+            for (let i of this.vertices[nextVertex].nodes) {
+                if (i.weight + this.vertices[i.nameOfVertex].weight < minWeigth) {
+                    minWeigth = this.vertices[i.nameOfVertex].weight;
+                    minVertex = i.nameOfVertex;
+                }
+            }
+            arrayWithVertex.push(minVertex);
+            nextVertex = minVertex;
+        }
+        return arrayWithVertex;
+    }
+
+
+    findShortestWay(start: string, finish: string): string[] {
+
+        let nodes: any = {};
+        let visitedVertex: string[] = [];
+
+        for (let i in this.vertices) {
+            if (this.vertices[i].name === start) {
+                this.vertices[i].weight = 0;
+
+            } else {
+                this.vertices[i].weight = Number.MAX_VALUE;
+            }
+            nodes[this.vertices[i].name] = this.vertices[i].weight;
+        }
+
+        while (Object.keys(nodes).length !== 0) {
+            let sortedVisitedByWeight: string[] = Object.keys(nodes).sort((a, b) => this.vertices[a].weight - this.vertices[b].weight);
+            let currentVertex: Vertex = this.vertices[sortedVisitedByWeight[0]];
+            for (let j of currentVertex.nodes) {
+                const calculateWeight: number = currentVertex.weight + j.weight;
+                if (calculateWeight < this.vertices[j.nameOfVertex].weight) {
+                    this.vertices[j.nameOfVertex].weight = calculateWeight;
+                }
+            }
+            delete nodes[sortedVisitedByWeight[0]];
+        }
+        const finishWeight: number = this.vertices[finish].weight;
+        let arrayWithVertex: string[] = this.findPointsOfShortestWay(start, finish, finishWeight).reverse();
+        arrayWithVertex.push(finish, finishWeight.toString());
+        return arrayWithVertex;
+    }
+
+}
+
+const graph = new Dijkstra();
+graph.addVertex(new Vertex(
+    "A", 
+    [
+        graph.setPathToNodes("B", 3), 
+        graph.setPathToNodes("C", 5)
+    ]
+    ));
+graph.addVertex(new Vertex(
+    "B", [
+        graph.setPathToNodes("A", 3),
+        graph.setPathToNodes("C", 2), 
+       graph.setPathToNodes("D", 6)
+    ]));
+graph.addVertex(new Vertex(
+    "C", 
+    [
+        graph.setPathToNodes("B", 2),
+        graph.setPathToNodes("A", 5),
+        graph.setPathToNodes("d", 5)
+    ]));
+graph.addVertex(new Vertex(
+    "D", 
+    [
+    graph.setPathToNodes("B",6),
+    graph.setPathToNodes("C", 5)
+    ]));
+
+console.log(graph.findShortestWay("A", "D"));
